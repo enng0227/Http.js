5c1decee572f51c9f615c73d42de080034c36029
twn39 <twn39@163.com>
2015-12-02 11:54:35 +0800

    update

--- dist/Http.js
+++ dist/Http.js
@@ -71,8 +71,77 @@ var Http = (function (window) {
         });
     }
 
+    /**
+     *
+     * @param url
+     * @param params
+     * @returns {Promise}
+     */
+    function put(url, params) {
+
+        return new Promise(function (resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+
+            var formData = new FormData();
+
+            for (var param in params) {
+                formData.append(param, params[param]);
+            }
+
+            var onreadystatechange = function onreadystatechange() {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    resolve(xhr);
+                }
+            };
+
+            xhr.open('PUT', url);
+            xhr.onreadystatechange = onreadystatechange;
+            xhr.send(formData);
+        });
+    }
+
+    /**
+     *
+     * @param url
+     * @param params
+     * @returns {Promise}
+     */
+    function del(url) {
+        var params = arguments.length <= 1 || arguments[1] === undefined ? undefined : arguments[1];
+
+        return new Promise(function (resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+
+            if (params !== undefined) {
+
+                var queryString = [];
+                for (var param in params) {
+                    queryString.push(param + '=' + params[param]);
+                }
+                queryString = queryString.join('&');
+                xhr.open('DELETE', url + '?' + queryString);
+            } else {
+
+                xhr.open('DELETE', url);
+            }
+
+            var onreadystatechange = function onreadystatechange() {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    resolve(xhr);
+                }
+            };
+
+            xhr.onreadystatechange = onreadystatechange;
+            xhr.send();
+        });
+    }
+
     return {
         "get": get,
-        "post": post
+        "post": post,
+        "put": put,
+        "delete": del
     };
 })(window);
--- src/Http.js
+++ src/Http.js
@@ -76,9 +76,84 @@ var Http = function (window) {
 
     }
 
+    /**
+     *
+     * @param url
+     * @param params
+     * @returns {Promise}
+     */
+    function put(url, params) {
+
+        return new Promise(function (resolve, reject) {
+
+            let xhr = new XMLHttpRequest();
+
+            let formData = new FormData();
+
+            for (var param in params) {
+                formData.append(param, params[param]);
+            }
+
+            let onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    resolve(xhr);
+                }
+
+            };
+
+            xhr.open('PUT', url);
+            xhr.onreadystatechange = onreadystatechange;
+            xhr.send(formData);
+
+        });
+
+    }
+
+    /**
+     *
+     * @param url
+     * @param params
+     * @returns {Promise}
+     */
+    function del(url, params = undefined) {
+
+        return new Promise(function (resolve, reject) {
+
+            let xhr = new XMLHttpRequest();
+
+            if (params !== undefined) {
+
+                let queryString = [];
+                for (var param in params) {
+                    queryString.push(param + '=' + params[param]);
+                }
+                queryString = queryString.join('&');
+                xhr.open('DELETE', url + '?' + queryString);
+
+            } else {
+
+                xhr.open('DELETE', url);
+
+            }
+
+
+            let onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    resolve(xhr);
+                }
+            };
+
+            xhr.onreadystatechange = onreadystatechange;
+            xhr.send();
+
+        });
+    }
+
     return {
         "get":get,
-        "post": post
+        "post": post,
+        "put":put,
+        "delete": del
     };
 
 } (window);
